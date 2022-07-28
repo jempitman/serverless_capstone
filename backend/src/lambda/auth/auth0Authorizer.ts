707875b05e2ffd02374a0ddfeb9e1adc12d29c82
jempitman <jempitman@gmail.com>
2022-07-28 16:45:46 +0200

    Feat: Get all Todos per user

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -2,25 +2,25 @@ import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
+// import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
-const logger = createLogger('auth')
+// const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-5d0trjd8.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
+//   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
+    // logger.info('User was authorized', jwtToken)
 
     return {
       principalId: jwtToken.sub,
@@ -36,7 +36,7 @@ export const handler = async (
       }
     }
   } catch (e) {
-    logger.error('User not authorized', { error: e.message })
+    // logger.error('User not authorized', { error: e.message })
 
     return {
       principalId: 'user',
@@ -56,12 +56,27 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
+    // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+
+  let response = await Axios.get(jwksUrl, {
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Credentials': true,
+        }
+  });
+
+  
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  
+  //extract signing key from jwks using getSigningKeys method
+  const signingKey = getSigningKeys(response.data, jwt)
+   
+  return verify(token, signingKey.publicKey, { algorithms: ['RS256'] }) as JwtPayload
+
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +89,32 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+//Retrieve signing key from jwks
+function getSigningKeys(jwks: any, jwt: Jwt){
+  const keys:any[] = jwks.keys
+
+  const signingKeys = keys
+  .filter(key => key.use === 'sig'  //set JWK for signing
+              && key.kty === 'RSA'  //Only RSA signing to be supported
+              && key.kid
+              && key.x5c && key.x5c.length
+  ).map(key => {
+    return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+  });
+
+  //extract final signing key from array of signing key according to key id (kid)
+  const finalSigningkey = signingKeys.find(key => key.kid === jwt.header.kid)
+
+  //return final signing key
+  return finalSigningkey
+}
+
+
+// construct signing certificate
+function certToPEM(cert: any){
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,70 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+import * as AWS from 'aws-sdk'
+// import { APIGateway } from 'aws-sdk'
+
+// import * as middy from 'middy'
+// import { cors } from 'middy/middlewares'
+
+// import { getAllTodos as getAllTodos } from '../../businessLogic/todos'
+// import { getUserId } from '../utils';
+// import { TodoItem } from '../../models/TodoItem'
 
 // TODO: Get all TODO items for a current user
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
 
-    return undefined
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):
+  Promise<APIGatewayProxyResult> => {
+
+    console.log('Processing event: ', event)
+    const result = await docClient.scan({
+      TableName: todosTable
+    }).promise()
+  
+
+    const items = result.Items
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        items
+      })
+    }
+
+  }
+
+
+
+// export const handler = middy(
+//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//     console.log('Caller event, ', event)
+//     // Write your code here
+    
+//     // const userId = getUserId(event);
+//     const todos = await getAllTodos(event)
+
+    
+
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         items: todos,
+//       })
+//     }
+// })
+
+
+// handler.use(
+//   cors({
+//     credentials: true
+//   })
+// )
