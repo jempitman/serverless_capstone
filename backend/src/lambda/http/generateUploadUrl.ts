db80c1177dcf3c00cb2a86dfa8e04dc09d802f5b
jempitman <jempitman@gmail.com>
2022-08-10 12:04:31 +0200

    Feat: validate generateUploadUrl requests

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-// import { generateUploadUrl } from '../../businessLogic/todos'
+import { getTodo } from '../../businessLogic/todos'
 import { updateAttachmentUrl } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
 import { createLogger } from '../../utils/logger'
@@ -19,9 +19,19 @@ export const handler = middy(
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
 
-    // const url = generateUploadUrl(todoId)
-    // console.log(`Upload url ${url} created`)
-    
+   try{
+
+    logger.info(`Verifying TodoItem with ID ${todoId} exists`)
+    const item = await getTodo(todoId, userId)
+
+      if(item.length === 0){
+        logger.error(`TodoItem with ID ${todoId} does not exist`)
+        return {
+            statusCode: 404,
+            body: 'Invalid todoId'
+        }
+      }
+
     const signedUrl = await updateAttachmentUrl(userId, todoId)
     console.log('update attachments url result ', signedUrl)
 
@@ -31,6 +41,10 @@ export const handler = middy(
             signedUrl
         )
   }
+    } catch (e: any){
+      logger.error(e.message)
+    }
+    
 })
 
 handler
