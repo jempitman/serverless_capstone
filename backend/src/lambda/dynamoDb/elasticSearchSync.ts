98a0c3be547726c95670b8c1c53c9930b5fb6184
jempitman <jempitman@gmail.com>
2022-08-25 11:45:03 +0200

    Feat: full text search for new todos

---
+++ backend/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -0,0 +1,48 @@
+import { DynamoDBStreamEvent, DynamoDBStreamHandler } from "aws-lambda"
+import 'source-map-support/register'
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
+
+const esHost = process.env.ES_ENDPOINT
+
+const es = new elasticsearch.Client({
+    hosts: [esHost],
+    connectionClass: httpAwsEs
+})
+
+
+
+export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
+    console.log('Processing events batch from DynamoDB', JSON.stringify(event))
+
+    for (const record of event.Records){
+        console.log('Processing record', JSON.stringify(record))
+
+        if (record.eventName !== 'INSERT'){
+            continue
+        }
+
+        const newTodoItem = record.dynamodb.NewImage
+        
+        const todoId = newTodoItem.todoId.S
+        // const userId = newTodoItem.userId.S
+
+        const body = {
+            todoId: newTodoItem.todoId.S,
+            userId: newTodoItem.userId.S,
+            dueDate: newTodoItem.dueDate.S,
+            createdAt: newTodoItem.createdAt.S,
+            name: newTodoItem.name.S,
+            done: newTodoItem.done.BOOL
+       }
+
+        await es.index({
+            index: 'todos-index',
+            type: 'todos',
+            id: todoId,
+            body
+        })
+
+    }
+
+}
